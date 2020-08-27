692ffbd0f9a06c3a2966cdbd2c5a4fe2eb4b393d
Sakshi Shanbhag <ssakshi.shanbhag6@gmail.com>
2020-08-27 17:59:14 +0530

    Update FollowersAnalysis.py

--- FollowersAnalysis.py
+++ FollowersAnalysis.py
@@ -11,10 +11,10 @@ DBSession = sessionmaker(bind=engine)
 sessionfollow = DBSession()
 
 
-consumer_key='hcoh0UTyaBrbCDP1QjKriOjXc'
-consumer_secret='Jtw3TB8CmyzUTlHkt4UHhvg3zcM1KMm4RBM9N2ebBxOedgnx8o'
-access_token='1136284891031597056-YLsUhgdABB6i7h4TSLOWvJFs7RlXxs'
-access_token_secret='QKXYm4F2j8l9jbxUz9jLPVnWVcJAxs7WQTeYPGOa7Wq5v'
+consumer_key=''
+consumer_secret=''
+access_token=''
+access_token_secret=''
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 auth_api = API(auth, wait_on_rate_limit=True)
